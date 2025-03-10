8f76f7d0f7cff70e082e0d1b30f55924cab325f8
harshithagp30 <harshithagp48@gmail.com>
2025-03-10 10:19:09 +0530

    Update Factorial.java

--- Factorial.java
+++ Factorial.java
@@ -2,7 +2,7 @@ public class Factorial {
 
     public static void main(String[] args) {
 
-        int num = 10;
+        int num = 4;
         long factorial = 1;
         for(int i = 1; i <= num; ++i)
         {
